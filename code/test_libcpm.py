660270b3f49f094e23c7f55a241ee6d9936d0946
Clément Ranc <clement.ranc@gmail.com>
2017-02-23 22:15:33 -0500

    Correction of python test module

--- code/test_libcpm.py
+++ code/test_libcpm.py
@@ -1,18 +1,6 @@
 # -*-coding:Utf-8 -*
 # ====================================================================
-# Copyright 2017 Clement Ranc
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied. See the License for the specific language governing
-# permissions and limitations under the License.
+# This routine tests the C++ routines
 # ====================================================================
 # Standard packages
 # ====================================================================
@@ -25,7 +13,7 @@ import libcpm
 # ====================================================================
 #   Functions
 # ====================================================================
-def test_magnifcalc(**kwargs):
+def run_test(**kwargs):
 
     s = np.linspace(0,10, 100)
     x = np.linspace(0, 10, 100)
@@ -40,7 +28,7 @@ def test_magnifcalc(**kwargs):
 # Main function
 # ====================================================================
 if __name__=="__main__":
-    test_magnifcalc()
+    run_test()
 
 
 
